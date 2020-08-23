ee82d7efd77b41a431b9259b0a13f406c851e97a
Celço Neto <celcoroxa@gmail.com>
2020-08-23 19:16:48 -0300

    studying nextjs with chakra ui

---
+++ pages/_documents.tsx
@@ -0,0 +1,26 @@
+import Document, {
+  DocumentProps,
+  Html,
+  Main,
+  NextScript,
+  Head,
+} from 'next/document';
+class MyDocuments extends Document<DocumentProps> {
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+export default MyDocuments;
